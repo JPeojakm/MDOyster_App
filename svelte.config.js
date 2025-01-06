9bafea09a098461556e4b5c9d747b8594732b1d8
Li Jinpeng <mr.lucas0514@gmail.com>
2025-01-06 00:58:11 -0600

    add adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter({
-			// 如果你希望支持单页面应用 (SPA)，使用 fallback 文件，如 app.html
-			fallback: 'app.html'
-		}),
-		paths: {
-			// 如果你的应用部署在子路径（如 https://username.github.io/repo-name），需要设置 base 路径
-			// 对于 Vercel 部署在根路径，注释掉以下行
-			// base: '/your-repo-name'
-		}
+			// see below for options that can be set here
+		})
 	}
 };
-
-export default config;
